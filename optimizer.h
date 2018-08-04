19100385b081b0842bf5dbef9c6179aa6d8c1d6f
谷秀青 <tsinggoo@guxiuqingdeMacBook-Pro.local>
2018-08-04 13:10:02 +0800

    add two files

---
+++ optimizer.h
@@ -0,0 +1,21 @@
+#ifndef OPTIMIZER_H
+#define OPTIMIZER_H
+
+#include <g2o/types/slam3d/types_slam3d.h>
+#include <g2o/core/sparse_optimizer.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/factory.h>
+#include <g2o/core/optimization_algorithm_factory.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/core/robust_kernel.h>
+#include <g2o/core/robust_kernel_factory.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/solvers/eigen/linear_solver_eigen.h>
+
+
+
+
+
+
+
+#endif
